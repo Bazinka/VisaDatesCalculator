82a99494badc3a38f1ccf02ef3f277a8c89e21d9
Daria Efimova <d.efimova@vectordigital.ru>
2019-03-25 14:40:16 +0100

    changed app style

--- app/src/main/java/com/visadatescalculator/MainActivity.kt
+++ app/src/main/java/com/visadatescalculator/MainActivity.kt
@@ -2,6 +2,9 @@ package com.visadatescalculator
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +12,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        // Setting up a back button
+        val navController = Navigation.findNavController(this, R.id.nav_host_fragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return Navigation.findNavController(this, R.id.nav_host_fragment).navigateUp()
+
     }
 }
